cee3fd7c99da1ff483e0a9071f34a3f007e610a9
mimyutt <mutiakhofifaturr@gmail.com>
2026-07-30 14:39:18 +0700

    menambahkan akses publik

--- IntroductionParadigma.cpp
+++ IntroductionParadigma.cpp
@@ -2,4 +2,15 @@
 using namespace std;
 
 class Mahasiswa{ //blue print atau cetakan
-};
+
+public :
+    int nim;
+    string nama;
+    float nilai;
+
+    void printData(){
+        cout << "NIM : " << nim << endl;
+        cout << "NAMA : " << nama << endl;
+        cout << "NILAI : " << nilai << endl;
+    }
+}; //batas class
